9b45fe291e1bdf2bda8aff05fcfe8103f53512e4
Yuuta Sakai <yuuta.sakai@wellsd.jp>
2025-05-13 09:38:58 +0900

    Create ARPaperDemo/ARPaperDemo.xcodeproj

---
+++ ARPaperDemo/ARPaperDemo/ARPaperDemoApp.swift
@@ -0,0 +1,33 @@
+//
+//  ARPaperDemoApp.swift
+//  ARPaperDemo
+//
+//  Created by 酒井雄太 on 2025/05/13.
+//
+
+import SwiftUI
+
+@main
+struct ARPaperDemoApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
