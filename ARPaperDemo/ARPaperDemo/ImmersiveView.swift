dfe513b585cc2f196e418813d9112529b74f0b3f
Yuuta Sakai <yuuta.sakai@wellsd.jp>
2025-05-19 09:54:21 +0900

    Update ImmersiveView.swift
    面が削除されない不具合修正
    - @State private var rootEntity = Entity() の導入
    - Viewの更新時に、planeEntitiesを全描画し直す

--- ARPaperDemo/ARPaperDemo/ImmersiveView.swift
+++ ARPaperDemo/ARPaperDemo/ImmersiveView.swift
@@ -13,6 +13,7 @@ import ARKit
 struct ImmersiveView: View {
     @State private var session = ARKitSession()
     @State private var planeEntities = [UUID: Entity]()   // Anchor ID ➜ Entity
+    @State private var rootEntity = Entity()
 
     var body: some View {
         RealityView { content in
@@ -20,17 +21,12 @@ struct ImmersiveView: View {
                !content.entities.contains(immersiveContentEntity) {
                 content.add(immersiveContentEntity)
             }
+            content.add(rootEntity)
         } update: { content in
-            // まだシーンに無いエンティティを追加
-            for (_, entity) in planeEntities where entity.parent == nil {
-                content.add(entity)
-            }
-            // planeEntities に存在しない子を削除
-            for child in content.entities {
-                guard let anchorID = UUID(uuidString: child.name) else { continue }
-                if planeEntities[anchorID] == nil {
-                    content.remove(child)
-                }
+            // planeEntities全てを再描画する(パフォーマンスの懸念あり)
+            rootEntity.children.removeAll()
+            for (_, entity) in planeEntities {
+                rootEntity.addChild(entity)
             }
         }
         .task { await runPlaneDetection() }
@@ -45,6 +41,7 @@ struct ImmersiveView: View {
                 switch update.event {
                 case .added, .updated:
                     if update.anchor.classification == .table {
+                        // .updated時も必ず新しいEntityを生成し直して差し替える
                         planeEntities[update.anchor.id] = makeEntity(for: update.anchor, color: .cyan.opacity(0.35))
                     }
                 case .removed:
@@ -64,13 +61,17 @@ struct ImmersiveView: View {
         )
         let material = SimpleMaterial(color: .init(color), roughness: 1, isMetallic: false)
         let entity = ModelEntity(mesh: mesh, materials: [material])
-        // 2 段行列で「エクステント中心 in World」を取得
+
+        // 「エクステント座標->世界座標変換行列」を取得
         let worldFromExtent =
                 anchor.originFromAnchorTransform
               * anchor.geometry.extent.anchorFromExtentTransform
         
         // XY平面メッシュを世界座標系における平面アンカーの位置・向きに移動
         entity.setTransformMatrix(worldFromExtent, relativeTo: nil)
+        
+        // anchorIDをEntity名に必ず設定
+        entity.name = anchor.id.uuidString
         return entity
     }
 }
